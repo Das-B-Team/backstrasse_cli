048a2a64f17744f0601f07563b195cf1f71204c3
vyemelya <55915562+vyemelya@users.noreply.github.com>
2020-01-07 16:51:44 +0100

    Updated

--- BackstrasseKonsole/BackstrasseKonsole/Ofen.cpp
+++ BackstrasseKonsole/BackstrasseKonsole/Ofen.cpp
@@ -3,13 +3,35 @@
 using namespace std;
 
 
-Ofen::Ofen() : OfenT {0.0}, BackraumL{0.0}
+Ofen::Ofen() : BackraumL{0.0}, OfenT {0.0}
 {
 }
 
-Ofen::Ofen(float T, float L) : OfenT {T}, BackraumL {L}
+Ofen::Ofen(float L, float T) : BackraumL {L}, OfenT {T}
 {
 }
+
+
+void Ofen::setLaenge(float L)
+{
+	BackraumL = L;
+}
+
+void Ofen::setTemperatur(float T)
+{
+	OfenT = T;
+}
+
+float Ofen::getLaenge()
+{
+	return BackraumL;
+}
+
+float Ofen::getTemperatur()
+{
+	return OfenT;
+}
+
 
 Ofen::~Ofen()
 {
