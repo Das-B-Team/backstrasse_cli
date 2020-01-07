eab2f863cc1e333b9e9d4dda15fcc3f39a1fe322
Grant Sut <grantsut@gmail.com>
2020-01-07 15:21:00 +0100

    cleaning up

--- BackstrasseKonsole/BackstrasseKonsole/BackstrasseKonsole.cpp
+++ BackstrasseKonsole/BackstrasseKonsole/BackstrasseKonsole.cpp
@@ -9,9 +9,9 @@ using namespace std;
 
 int main()
 {
-	locale::global(locale(""));  // f�r deutsche Umlaute
-	cout.imbue(locale(""));  // f�r das Komma
-	cin.imbue(locale(""));  // f�r das Komma
+	locale::global(locale(""));
+	cout.imbue(locale(""));
+	cin.imbue(locale(""));
 
 	Auftrag* a = new Auftrag();
 
@@ -26,16 +26,15 @@ int main()
 		cout << e.what() << endl;
 	}
 
+	cout << "Mehl = " << a->getGesamtMehl() << endl;
+	cout << "Milch = " << a->getGesamtMilch() << endl;
+	cout << "Eier = " << a->getGesamtEier() << endl;
+	cout << "Backpulver = " << a->getGesamtBackpulver() << endl;
+	cout << "Zucker = " << a->getGesamtZucker() << endl;
+	cout << "Nuesse = " << a->getGesamtNuesse() << endl;
+	cout << "Kakao = " << a->getGesamtKakao() << endl;
 
-	//cout << "Mehl = " << a->getGesamtMehl() << endl;
-	//cout << "Milch = " << a->getGesamtMilch() << endl;
-	//cout << "Eier = " << a->getGesamtEier() << endl;
-	//cout << "Backpulver = " << a->getGesamtBackpulver() << endl;
-	//cout << "Zucker = " << a->getGesamtZucker() << endl;
-	//cout << "Nuesse = " << a->getGesamtNuesse() << endl;
-	//cout << "Kakao = " << a->getGesamtKakao() << endl;
-
-	//cout << endl;
+	cout << endl;
 
    
   //  Datei* my_datei = new Datei("b_team_config.txt");
