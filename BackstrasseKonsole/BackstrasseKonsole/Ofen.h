f71c4de64ba350adc6f8eddb0b7a37e9ed3df907
vyemelya <55915562+vyemelya@users.noreply.github.com>
2020-01-07 16:44:37 +0100

    updated set/get methoden

--- BackstrasseKonsole/BackstrasseKonsole/Ofen.h
+++ BackstrasseKonsole/BackstrasseKonsole/Ofen.h
@@ -1,16 +1,21 @@
 #pragma once
 class Ofen
 {
-	//Ofen Parameter, T=Temperatur und L=Laenge würde deklariert 
+	//Ofen Parameter, T=Temperatur und L=Laenge wÃ¼rde deklariert 
 
-	float OfenT;
-	float BackraumL;
+	float OfenTemperatur;
+	float BackraumLaenge;
 
 
 public:
       
 	Ofen();
 	Ofen(float, float);
+	void setLaenge(float);
+        void setTemperatur(float);
+
+	float getLaenge();
+	float getTemperatur();
 	~Ofen();
 };
 
