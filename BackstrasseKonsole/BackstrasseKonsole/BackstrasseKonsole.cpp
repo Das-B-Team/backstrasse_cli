0754c24fa73e40dc3b2358576b09f51564199746
Grant Sut <grantsut@gmail.com>
2020-01-07 15:18:28 +0100

    try catch block added

--- BackstrasseKonsole/BackstrasseKonsole/BackstrasseKonsole.cpp
+++ BackstrasseKonsole/BackstrasseKonsole/BackstrasseKonsole.cpp
@@ -15,17 +15,25 @@ int main()
 
 	DateiHandel d("b_team_config.txt");
 
-	d.dateiLesen(a);
+	try
+	{
+		d.dateiLesen(a);
+	}
+	catch (exception e)
+	{
+		cout << e.what() << endl;
+	}
 
-	cout << "Mehl = " << a->getGesamtMehl() << endl;
-	cout << "Milch = " << a->getGesamtMilch() << endl;
-	cout << "Eier = " << a->getGesamtEier() << endl;
-	cout << "Backpulver = " << a->getGesamtBackpulver() << endl;
-	cout << "Zucker = " << a->getGesamtZucker() << endl;
-	cout << "Nuesse = " << a->getGesamtNuesse() << endl;
-	cout << "Kakao = " << a->getGesamtKakao() << endl;
 
-	cout << endl;
+	//cout << "Mehl = " << a->getGesamtMehl() << endl;
+	//cout << "Milch = " << a->getGesamtMilch() << endl;
+	//cout << "Eier = " << a->getGesamtEier() << endl;
+	//cout << "Backpulver = " << a->getGesamtBackpulver() << endl;
+	//cout << "Zucker = " << a->getGesamtZucker() << endl;
+	//cout << "Nuesse = " << a->getGesamtNuesse() << endl;
+	//cout << "Kakao = " << a->getGesamtKakao() << endl;
+
+	//cout << endl;
 
     return 0; 
 }
